da81ff5adee0f0d2795f088f093a7bcb79cb8e0a
Lily Fitz <104954859+lily-fitz@users.noreply.github.com>
2023-02-28 00:56:29 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className='container section'>
       <h6>&copy; Lily Fitz</h6>
-      <h6>The Best Blog</h6>
+      <h6>The Best Blog.</h6>
     </footer>
   )
 }
